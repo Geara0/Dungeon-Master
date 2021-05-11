6cf99f2948029b4fb587179a45acbdee01cd45bb
JoBeR007 <kdv200265@gmail.com>
2021-05-11 19:06:43 +0500

    Revert "Merge pull request #9 from Geara0/Geara0"
    This reverts commit b660a006bbc90a7bbe978a4958604dcffedc68c6, reversing
    changes made to bbd25dc05bfbd4aa39be12d39b76d8d032cea086.

--- DungeonMaster/Assets/Scripts/DotBehaviour.cs
+++ DungeonMaster/Assets/Scripts/DotBehaviour.cs
@@ -7,17 +7,23 @@ public class DotBehaviour : MonoBehaviour
 {
     private float nextActionTime;
     public float fireRate = 1;
-    public int price = 200;
+    private float projectileDivisor = 2;
     
     public ProjectileBehaviour projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
     void Update()
     {
         if (Time.time > nextActionTime)
         {
             nextActionTime += 1 / fireRate;
-            var xOffset = transform.right * (transform.localScale.x / 2);
-            var yOffset = transform.up * (transform.localScale.y / 2);
+            var xOffset = transform.right * transform.localScale.x;
+            var yOffset = transform.up * transform.localScale.y;
 
             Instantiate(projectilePrefab, transform.position + xOffset, Quaternion.Euler(0f, 0f, 0f));
             Instantiate(projectilePrefab, transform.position + yOffset, Quaternion.Euler(0f, 0f, 90f));
--- DungeonMaster/Assets/Scripts/ProjectileBehaviour.cs
+++ DungeonMaster/Assets/Scripts/ProjectileBehaviour.cs
@@ -6,7 +6,13 @@ using UnityEngine;
 public class ProjectileBehaviour : MonoBehaviour
 {
     public float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
     
+    // Update is called once per frame
     void Update()
     {
         transform.position += transform.right * (Time.deltaTime * speed);
