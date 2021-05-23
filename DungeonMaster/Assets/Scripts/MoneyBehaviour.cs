76060fdfff350ab084a69cdf1715653942e4b78b
Geara0 <adrezz.26.5@gmail.com>
2021-05-23 22:34:10 +0500

    Money disappear

--- DungeonMaster/Assets/Scripts/MoneyBehaviour.cs
+++ DungeonMaster/Assets/Scripts/MoneyBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,24 @@ using UnityEngine;
 public class MoneyBehaviour : MonoBehaviour
 {
     public int cost = 20;
-    
+    public float DisappearTime = 16;
+
+    private void Start()
+    {
+        StartCoroutine(nameof(Disappear));
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         var player = other.collider.GetComponent<PlayerController>();
         if (!player) return;
         PlayerController.GainMoney(cost);
+        Destroy(gameObject);
+    }
+
+    private IEnumerator Disappear()
+    {
+        yield return new WaitForSeconds(DisappearTime);
         Destroy(gameObject);
     }
 }
